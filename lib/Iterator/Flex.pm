e5641127055e572074907d2cc8ef20c1485a1e38
Diab Jerius <djerius@cfa.harvard.edu>
2018-01-05 17:19:48 -0500

    bump version

--- lib/Iterator/Flex.pm
+++ lib/Iterator/Flex.pm
@@ -5,7 +5,7 @@ package Iterator::Flex;
 use strict;
 use warnings;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use Exporter 'import';
 
--- lib/Iterator/Flex/Iterator.pm
+++ lib/Iterator/Flex/Iterator.pm
@@ -5,7 +5,7 @@ package Iterator::Flex::Iterator;
 use strict;
 use warnings;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use Carp ();
 use Ref::Util;
