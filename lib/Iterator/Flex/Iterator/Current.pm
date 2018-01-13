363f4105c36d5f8893af133153cd68dc6c917059
Diab Jerius <djerius@cfa.harvard.edu>
2018-01-12 19:14:12 -0500

    oops. forgot some files

---
+++ lib/Iterator/Flex/Iterator/Current.pm
@@ -0,0 +1,26 @@
+package Iterator::Flex::Iterator::Current;
+
+# ABSTRACT: Role to add current method to an Iterator::Flex::Iterator
+
+use strict;
+use warnings;
+
+our $VERSION = '0.02';
+
+use Role::Tiny;
+
+=method current
+
+=method __current__
+
+   $iterator->current;
+
+Returns the current value.
+
+=cut
+
+sub current { local $_ = $_[0]; $_->{current}->() }
+*__current__ = \&current;
+
+
+1;
---
+++ lib/Iterator/Flex/Iterator/Reset.pm
@@ -0,0 +1,50 @@
+package Iterator::Flex::Iterator::Reset;
+
+# ABSTRACT: Role to add reset capability to an Iterator::Flex::Iterator
+
+use strict;
+use warnings;
+
+our $VERSION = '0.02';
+
+use Carp       ();
+use List::Util ();
+
+use Role::Tiny;
+
+=method reset
+
+=method __reset__
+
+   $iterator->reset;
+
+Resets the iterator to its initial value.
+
+=cut
+
+sub reset {
+
+    my $self = shift;
+
+    if ( defined $self->{depends} ) {
+
+        # first check if dependencies can reset.
+        my $cant
+          = List::Util::first { !$_->can( 'reset' ) } @{ $self->{depends} };
+        Carp::croak( "dependency: @{[ $cant->{name} ]} is not resetable\n" )
+          if $cant;
+
+        # now reset them
+        $_->reset foreach @{ $self->{depends} };
+    }
+
+    local $_ = $self;
+    $self->{reset}->();
+
+    return;
+}
+*__reset__ = \&reset;
+
+
+
+1;
